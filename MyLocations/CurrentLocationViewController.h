a6e36613e648792c96a26272dded38eb1e0903d6
Ivan Magda <imagda15@gmail.com>
2014-10-07 21:26:26 +1000

    Get current location

--- MyLocations/CurrentLocationViewController.h
+++ MyLocations/CurrentLocationViewController.h
@@ -1,15 +1,19 @@
-//
-//  FirstViewController.h
-//  MyLocations
-//
-//  Created by Ivan Magda on 06.10.14.
-//  Copyright (c) 2014 Ivan Magda. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface CurrentLocationViewController : UIViewController
+                                           <CLLocationManagerDelegate>
 
+@property (nonatomic, weak) IBOutlet UILabel *messageLabel;
+@property (nonatomic, weak) IBOutlet UILabel *latitudeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *longitudeLabel;
+@property (nonatomic, weak) IBOutlet UILabel *addressLabel;
+
+@property (nonatomic, weak) IBOutlet UIButton *tagButton;
+@property (nonatomic, weak) IBOutlet UIButton *getButton;
+
+
+- (IBAction)getLocation:(id)sender;
 
 @end
 
--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -1,27 +1,44 @@
-//
-//  FirstViewController.m
-//  MyLocations
-//
-//  Created by Ivan Magda on 06.10.14.
-//  Copyright (c) 2014 Ivan Magda. All rights reserved.
-//
-
 #import "CurrentLocationViewController.h"
 
 @interface CurrentLocationViewController ()
 
 @end
 
-@implementation CurrentLocationViewController
+@implementation CurrentLocationViewController {
+    CLLocationManager *_locationManager;
+}
+
+#pragma mark - View Controller LifeCycle -
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if ((self = [super initWithCoder:aDecoder])) {
+        _locationManager = [[CLLocationManager alloc]init];
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - IBActions -
+
+- (IBAction)getLocation:(id)sender {
+    _locationManager.delegate = self;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    [_locationManager startUpdatingLocation];
+}
+
+#pragma mark - CLLocationManagerDelegate -
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"didFailWithError %@", error);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *newLocation = [locations lastObject];
+    NSLog(@"didUpdateLocations %@", newLocation);
+
 }
 
 @end
