d118e2879da78305fc3ae6270d451f26939ff3ba
Ivan Magda <imagda15@gmail.com>
2014-10-12 22:09:51 +1000

    Obtained the current location using the device services. When app can't handle coordinates uses errors handlers

--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -6,6 +6,9 @@
 
 @implementation CurrentLocationViewController {
     CLLocationManager *_locationManager;
+    CLLocation        *_location;
+    BOOL               _updatingLocation;
+    NSError           *_lastLocationError;
 }
 
 #pragma mark - View Controller LifeCycle -
@@ -19,26 +22,89 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [self updateLabels];
 }
 
 #pragma mark - IBActions -
 
 - (IBAction)getLocation:(id)sender {
-    _locationManager.delegate = self;
-    _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
-    [_locationManager startUpdatingLocation];
+    [self startLocationManager];
+    [self updateLabels];
 }
 
 #pragma mark - CLLocationManagerDelegate -
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
     NSLog(@"didFailWithError %@", error);
+
+    if (error.code == kCLErrorLocationUnknown) {
+        return;
+    }
+
+    [self stopLocationManager];
+    _lastLocationError = error;
+
+    [self updateLabels];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
     CLLocation *newLocation = [locations lastObject];
+
     NSLog(@"didUpdateLocations %@", newLocation);
 
+    _lastLocationError = nil;
+    _location = newLocation;
+    [self updateLabels];
+}
+
+- (void)updateLabels {
+    if (_location) {
+        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", _location.coordinate.latitude];
+        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", _location.coordinate.longitude];
+        self.tagButton.hidden = NO;
+        self.messageLabel.text = @"";
+    } else {
+        self.latitudeLabel.text = @"";
+        self.longitudeLabel.text = @"";
+        self.addressLabel.text = @"";
+        self.tagButton.hidden = YES;
+
+        NSString *statusMessage;
+        if (_lastLocationError) {
+            if ([_lastLocationError.domain isEqualToString: kCLErrorDomain] &&
+                _lastLocationError.code == kCLErrorDenied) {
+                statusMessage = @"Location Services Disabled";
+            } else {
+                statusMessage = @"Error Getting Location";
+            }
+        } else if (![CLLocationManager locationServicesEnabled]) {
+            statusMessage = @"Location Services Disabled";
+        } else if (_updatingLocation) {
+                statusMessage = @"Searching...";
+        } else {
+                statusMessage = @"Press the Button to Start";
+        }
+        self.messageLabel.text = statusMessage;
+    }
+}
+
+- (void)startLocationManager {
+    if ([CLLocationManager locationServicesEnabled]) {
+        _locationManager.delegate = self;
+        _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        [_locationManager requestWhenInUseAuthorization];
+        [_locationManager startUpdatingLocation];
+        _updatingLocation = YES;
+    }
+}
+
+- (void)stopLocationManager {
+    if (_updatingLocation) {
+        [_locationManager stopUpdatingLocation];
+        _locationManager.delegate = nil;
+        _updatingLocation = NO;
+    }
 }
 
 @end
