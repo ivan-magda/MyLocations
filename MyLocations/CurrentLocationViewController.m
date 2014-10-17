0f2701554682c9ea71f5c8f92c69e0a20d894860
Ivan Magda <imagda15@gmail.com>
2014-10-17 10:28:13 +1000

    Testing in practice

--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -5,6 +5,7 @@
 @end
 
 @implementation CurrentLocationViewController {
+        //get location
     CLLocationManager *_locationManager;
     CLLocation        *_location;
     BOOL               _updatingLocation;
@@ -13,7 +14,8 @@
         //reverse coding
     CLGeocoder *_geocoder; //is the object that will perform the geocoding
     CLPlacemark *_placemark; //is the object that contains the address results
-    BOOL _performingReverseGeocoding; //set to YES when a geocoding operation is taking place
+    BOOL _performingReverseGeocoding; //set to YES when a geocoding operation is
+                                      //taking place
     NSError *_lastGeocodingError;
 }
 
@@ -49,7 +51,6 @@
 
         [self startLocationManager];
     }
-
     [self updateLabels];
     [self configureGetButton];
 }
@@ -84,6 +85,12 @@
     if (newLocation.horizontalAccuracy < 0) {
         return;
     }
+        //calculates the distance between the new reading and the previous reading,
+        //if there was one
+    CLLocationDistance distance = MAXFLOAT;
+    if (_location) {
+        distance = [newLocation distanceFromLocation:_location];
+    }
 
         //if the new reading coordinates is more useful than the previous one
         //larger accuracy value actually means less accurate
@@ -96,6 +103,12 @@
             NSLog(@"We're done!");
             [self stopLocationManager];
             [self configureGetButton];
+
+                //if distance is 0, then this location is the same as the location from
+                //a previous reading and you don’t need to reverse geocode it anymore
+            if (distance > 0) {
+                _performingReverseGeocoding = NO;
+            }
         }
 
         if (!_performingReverseGeocoding) {
@@ -107,31 +120,45 @@
                 //following completionHandler should be executed as soon as the
                 //geocoding is completed
             [_geocoder reverseGeocodeLocation:_location
-                            completionHandler:
-             ^(NSArray *placemarks, NSError *error) {
-                 NSLog(@"*** Found placemarks: %@, error: %@",placemarks, error);
+                            completionHandler:^(NSArray *placemarks, NSError *error) {
+                                NSLog(@"*** Found placemarks: %@, error: %@",placemarks, error);
 
-                 _lastGeocodingError = error;
-                 if (error == nil && [placemarks count] > 0) {
-                     _placemark = [placemarks lastObject];
-                 } else {
-                     _placemark = nil;
-                 }
-
-                 _performingReverseGeocoding = NO;
-                 [self updateLabels];
-             }];
+                                _lastGeocodingError = error;
+                                if (error == nil && [placemarks count] > 0) {
+                                    _placemark = [placemarks lastObject];
+                                } else {
+                                    _placemark = nil;
+                                }
+                                
+                                _performingReverseGeocoding = NO;
+                                [self updateLabels];
+                            }];
+        }
+            //If the coordinate from this reading is not significantly different from
+            //the previous reading and it has been more than 10 seconds since you’ve
+            //received that original reading, then it’s a good point to hang up your
+            //hat and stop.
+    } else if (distance < 1.0) {
+        NSTimeInterval timeInterval = [newLocation.timestamp timeIntervalSinceDate:_location.timestamp];
+        if (timeInterval > 10) {
+            NSLog(@"*** Force done!");
+            [self stopLocationManager];
+            [self updateLabels];
+            [self configureGetButton];
         }
     }
 }
 
+#pragma mark - UI methods -
+
 - (NSString *)stringFromPlacemark:(CLPlacemark *)thePlacemark {
     return [NSString stringWithFormat:@"%@ %@\n%@ %@ %@",
-            thePlacemark.subThoroughfare,
-            thePlacemark.thoroughfare,
-            thePlacemark.locality,
-            thePlacemark.administrativeArea,
-            thePlacemark.postalCode];
+            thePlacemark.subThoroughfare,    //is the house number
+            thePlacemark.thoroughfare,       //is the street name
+            thePlacemark.locality,           //is the city
+            thePlacemark.administrativeArea, //is the state or province
+            thePlacemark.postalCode          //is the zip code or postal code
+            ];
 }
 
 - (void)updateLabels {
@@ -183,6 +210,8 @@
     }
 }
 
+#pragma mark - Configure locationManager -
+
 - (void)startLocationManager {
     if ([CLLocationManager locationServicesEnabled]) {
         _locationManager.delegate = self;
@@ -190,14 +219,32 @@
         [_locationManager requestWhenInUseAuthorization];
         [_locationManager startUpdatingLocation];
         _updatingLocation = YES;
+
+        [self performSelector:@selector(didTimeOut:) withObject:nil afterDelay:60];
     }
 }
 
 - (void)stopLocationManager {
     if (_updatingLocation) {
+        [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(didTimeOut:) object:nil];
+
         [_locationManager stopUpdatingLocation];
         _locationManager.delegate = nil;
         _updatingLocation = NO;
+    }
+}
+
+- (void)didTimeOut:(id)obj {
+    NSLog(@"*** Time out");
+
+    if (!_location) {
+        [self stopLocationManager];
+
+        _lastLocationError = [NSError errorWithDomain:@"MyLocationsErrorDomain"
+                                                 code:1
+                                             userInfo:nil];
+        [self updateLabels];
+        [self configureGetButton];
     }
 }
 
