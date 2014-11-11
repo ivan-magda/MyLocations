3f4dfc6f4d7ebdad6ce013d1aa48894855797806
Ivan Magda <imagda15@gmail.com>
2014-11-11 10:48:44 +0900

    Showing picked UIImage.

--- MyLocations/LocationDetailsViewController.m
+++ MyLocations/LocationDetailsViewController.m
@@ -26,6 +26,9 @@ extern NSString * const ManagedObjectContextSaveDidFailNotification;
 @property (nonatomic, weak) IBOutlet UILabel *addressLabel;
 @property (nonatomic, weak) IBOutlet UILabel *dateLabel;
 
+@property (nonatomic, weak) IBOutlet UIImageView *imageView;
+@property (nonatomic, weak) IBOutlet UILabel *imageLabel;
+
 @end
 
 #pragma mark - Class Implementation
@@ -34,6 +37,8 @@ extern NSString * const ManagedObjectContextSaveDidFailNotification;
     NSString *_descriptionText;
     NSString *_categoryName;
     NSDate *_date;
+
+    UIImage *_image;
 }
 
 #pragma mark - ViewController Life Cycle -
@@ -90,6 +95,13 @@ extern NSString * const ManagedObjectContextSaveDidFailNotification;
 
 #pragma mark - UIImagePickerController -
 
+- (void)showImage:(UIImage *)image {
+    self.imageView.image = _image;
+    self.imageView.hidden = NO;
+    self.imageView.frame = CGRectMake(10, 10, 260, 260);
+    self.imageLabel.hidden = YES;
+}
+
 - (void)takePhoto {
     UIImagePickerController *imagePickerController = [[UIImagePickerController alloc]init];
 
@@ -128,6 +140,11 @@ extern NSString * const ManagedObjectContextSaveDidFailNotification;
 #pragma mark UIImagePickerControllerDelegate
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    _image = info[UIImagePickerControllerEditedImage];
+
+    [self showImage:_image];
+    [self.tableView reloadData];
+
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -140,6 +157,12 @@ extern NSString * const ManagedObjectContextSaveDidFailNotification;
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.section == 0 && indexPath.row == 0) {
         return 88;
+    } else if (indexPath.section == 1) {
+        if (self.imageView.hidden) {
+            return 44;
+        } else {
+            return 280;
+        }
     } else if (indexPath.section == 2 && indexPath.row == 2) {
         CGRect rect = CGRectMake(100, 10, 205, 10000);
         self.addressLabel.frame = rect;
